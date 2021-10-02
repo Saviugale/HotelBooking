ca2e781392b0f8bc67d5e867c000f2d87e69b99c
Savitri Maruti Ugale <91776735+Saviugale@users.noreply.github.com>
2021-10-02 19:44:11 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <hr />
+            <center>
+                <h3>&copy;Savitri Ugale</h3>
+            </center>
+
+        </footer>
+    )
+}
+
+export default Footer
